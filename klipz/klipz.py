a7b3c69893fa7e597d1f447bff3d412676af9d42
Jacob Appelbaum <jacob@appelbaum.net>
2020-05-11 16:14:45 +0200

    begin to tackle pylint review

--- klipz/klipz.py
+++ klipz/klipz.py
@@ -1,9 +1,10 @@
+"""
+klipz is a python module for saving clippings.
+"""
 import argparse
 import ast
 import curses
 import os
-import pkg_resources
-import pyperclip
 import re
 import signal
 import socket
@@ -11,6 +12,8 @@ import subprocess
 import sys
 import tempfile
 import time
+import pkg_resources
+import pyperclip
 
 # consts
 SAVED_HEADER = "Saved Clippings"
@@ -53,6 +56,9 @@ def command_line_arguments():
 
 
 def main():
+    """
+    the primary klipz entry point
+    """
     global cmdline
     ap = command_line_arguments()
     cmdline = ap.parse_args()
@@ -66,6 +72,9 @@ def main():
 
 
 def register_default_keys():
+    """
+    register map of hotkeys to functions.
+    """
     register_key("e", call_editor)
     register_key("s", toggle_saved, None)
     register_key("u", move_up, None)
@@ -77,6 +86,9 @@ def register_default_keys():
 
 
 def read_config_file():
+    """
+    read klipz configuration file.
+    """
     try:
         exec(open(os.path.expanduser(cmdline.configdir + "/config.py")).read())
     except FileNotFoundError:
@@ -84,6 +96,9 @@ def read_config_file():
 
 
 def saved_to_disk():
+    """
+    Write klipz saved clips to disk.
+    """
     try:
         os.mkdir(os.path.expanduser(cmdline.configdir))
     except FileExistsError:
@@ -97,6 +112,9 @@ def saved_to_disk():
 
 
 def saved_from_disk():
+    """
+    Load saved klipz clips from disk.
+    """
     global saved_clips
     try:
         with open(os.path.expanduser(cmdline.configdir + "/saved_clips"),
@@ -108,13 +126,16 @@ def saved_from_disk():
 
 
 def worker(scr):
+    """
+    Primary klipz worker loop.
+    """
     global compare, displayed, selected, screen
     screen = scr
     curses.use_default_colors()
     screen.timeout(0)
     redraw()
     signal.signal(signal.SIGWINCH, handle_resize)
-    signal.signal(signal.SIGTERM, quit)
+    signal.signal(signal.SIGTERM, _quit)
     while not quitting:
         try:
             while True:
@@ -140,17 +161,24 @@ def worker(scr):
             else:
                 time.sleep(0.1)
         except KeyboardInterrupt:
-            quit()
+            _quit()
         except:
             pass
 
 
-def quit(signum=None, frame=None):
+def _quit(signum=None, frame=None):
+    """
+    *_quit* is called by signal handlers as well as other callers who want to
+    exit.
+    """
     global quitting
     quitting = True
 
 
 def execute_function(tuple):
+    """
+    Execute a given function.
+    """
     global displayed
     if not tuple:
         return
@@ -165,10 +193,14 @@ def execute_function(tuple):
         args.pop(0)
         displayed[selected] = clip
         copy_to_clipboard(clip)
+        offset_in_clip = 0
         redraw()
 
 
 def handle_resize(signum, frame):
+    """
+    *handle_resize* is used for handling a terminal resize.
+    """
     global SIGWINCH_works, screen, bottom
     if signum or frame:
         SIGWINCH_works = True
@@ -177,10 +209,14 @@ def handle_resize(signum, frame):
     screen.clear()
     curses.resizeterm(*screen.getmaxyx())
     bottom = max(0, selected - curses.LINES + 1)
+    offset_in_clip = 0
     redraw()
 
 
 def redraw():
+    """
+    *redraw* redraws the screen.
+    """
     global width
     screen.clear()
     width = curses.COLS - 1
@@ -213,6 +249,9 @@ def redraw():
 
 
 def up():
+    """
+    *up* moves a selected item up in the list of clips.
+    """
     global selected, bottom, offset_in_clip
     if selected < len(displayed) - 1:
         selected += 1
@@ -227,6 +266,9 @@ def up():
 
 
 def down():
+    """
+    *down* moves a selected item down in the list of clips.
+    """
     global selected, bottom, offset_in_clip
     if selected > 0:
         selected -= 1
@@ -238,6 +280,9 @@ def down():
 
 
 def scroll_left():
+    """
+    *scroll_left* scrolls the text for an item left.
+    """
     global offset_in_clip
     offset_in_clip -= width
     offset_in_clip = max(0, offset_in_clip)
@@ -245,6 +290,9 @@ def scroll_left():
 
 
 def scroll_right():
+    """
+    *scroll_right* scrolls the text for an item left.
+    """
     global offset_in_clip
     offset_in_clip += width
     offset_in_clip = min(len(displayed[selected]) - width, offset_in_clip)
@@ -253,6 +301,9 @@ def scroll_right():
 
 
 def move_down():
+    """
+    *move_down* moves an item down.
+    """
     global selected
     if pastes is displayed:
         return
@@ -264,6 +315,9 @@ def move_down():
 
 
 def move_up():
+    """
+    *move_up* moves an item up.
+    """
     global has_upped, selected
     if pastes is displayed:
         return
@@ -280,6 +334,9 @@ def move_up():
 
 
 def toggle_saved():
+    """
+    *toggle_saved* toggles saving to disk.
+    """
     global selected, bottom, has_upped, offset_in_clip, displayed
     if saved_clips is displayed:
         saved_to_disk()
@@ -295,12 +352,18 @@ def toggle_saved():
 
 
 def copy_to_clipboard(s):
+    """
+    *copy_to_clipboard* copies to the clipboard.
+    """
     global compare
     pyperclip.copy(s)
     compare = s
 
 
 def register_key(key, func=None, args=[]):
+    """
+    *register_key* registers a hotkey to a function.
+    """
     global registered_keys
     if type(key) == str:
         key = ord(key)
@@ -311,6 +374,9 @@ def register_key(key, func=None, args=[]):
 
 
 def pass_as_tempfile(clip, command_and_args):
+    """
+    *pass_as_tempfile* passes a tempfile.
+    """
     global screen
     curses.endwin()
     with tempfile.NamedTemporaryFile(suffix=".tmp", delete=False) as tf_out:
@@ -325,6 +391,9 @@ def pass_as_tempfile(clip, command_and_args):
 
 
 def pipe_through(clip, command_and_args):
+    """
+    *pipe_through* pipes *clip* through *command_and_args*.
+    """
     if type(command_and_args) == str:
         command_and_args = [command_and_args]
     p = subprocess.Popen(command_and_args, stdout=subprocess.PIPE,
@@ -334,6 +403,9 @@ def pipe_through(clip, command_and_args):
 
 
 def call_editor(clip):
+    """
+    *call_editor* uses $EDITOR to edit the clip list.
+    """
     editor = os.environ.get("EDITOR", None)
     if editor:
         clip = pass_as_tempfile(clip, [editor])
@@ -341,14 +413,19 @@ def call_editor(clip):
 
 
 def cutcrlf(clip):
+    """
+    *cutcrlf* edits *clip* to remove CRLF.
+    """
     if not cmdline.leavecrlf:
         clip = re.sub("^[\r\n]+", "", clip)
         clip = re.sub("[\r\n]+$", "", clip)
     return clip
 
 
-# For later
 def client(ip, port, message):
+    """
+    *client* is for later.
+    """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((ip, port))
         sock.sendall(bytes(message, "ascii"))
